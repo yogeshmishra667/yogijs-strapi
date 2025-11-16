6c6652b98082b62b5376d134feab6e5c5c8369b5
yogesh kumar mishra <yogeshmishra667@gmail.com>
2025-11-16 18:19:19 +0530

    intial strapi structure

---
+++ src/api/user-detail/controllers/user-detail.ts
@@ -0,0 +1,7 @@
+/**
+ * user-detail controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::user-detail.user-detail');
---
+++ src/api/user-detail/routes/user-detail.ts
@@ -0,0 +1,7 @@
+/**
+ * user-detail router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::user-detail.user-detail');
